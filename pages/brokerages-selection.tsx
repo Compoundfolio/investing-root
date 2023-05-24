727eb154d40d2865c40f26c405bd4872b03f0b84
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-05-25 00:17:11 +0400

    DOne next 13 migration (without turbopack)

--- pages/brokerages-selection.tsx
+++ pages/brokerages-selection.tsx
@@ -3,17 +3,17 @@ import Head from "next/head"
 import BrokerageReportUploadPage from 'src/components/pages/BrokerageReportUploadPage';
 
 const BrokerageSelection = () => {
-  // const { data: session } = useSession()
+  const { data: session } = useSession()
   // const { accessToken } = session
 
-  // if (session) {
-  //   return (
-  //     <>
-  //       Signed in as {session.user.email} <br />
-  //       <button onClick={() => signOut()}>Sign out</button>
-  //     </>
-  //   )
-  // }
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
     <>
       <Head>
