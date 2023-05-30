b629a83feb172312210c431a1ef4c5e38049a308
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-05-31 01:22:43 +0400

    Migration to server-client components stage 1

--- src/core/components/help/ShortcutHelper/ShortcutHelper.tsx
+++ src/core/components/help/ShortcutHelper/ShortcutHelper.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { memo, useCallback, useEffect, useState, useMemo } from 'react'
 import { IShortcutHelper, KeyShortcut } from './types'
 import { isMacOs } from '@core/helpers'
