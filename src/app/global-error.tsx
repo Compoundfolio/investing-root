d4810d864e8f84a3f84ddd50b0c0d227890247bd
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-10-16 10:36:12 +0200

    Add global err boundary screen base, add background for error pages

---
+++ src/app/global-error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function GlobalError() {
+  return <h2>Something went wrong!</h2>
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,14 @@ import { AppLogo } from "@core"
 import React from "react"
 import clsx from "clsx"
 import styles from "./not-found.module.css"
+import Image from "next/image"
 
 export default function NotFound() {
   return (
     <>
-      <div className="relative z-10 flex flex-col items-center justify-between w-full h-full fadeIn">
+      <div className="relative flex flex-col items-center justify-between w-full h-full fadeIn">
         <AppLogo withTitle />
-        <section className="flex flex-col">
+        <section className="z-10 flex flex-col">
           <span className={clsx(styles.coreText, styles.notFoundPage_subTitle)}>
             404
           </span>
@@ -25,9 +26,17 @@ export default function NotFound() {
             Error
           </span>
         </section>
-        <footer></footer>
+        <footer className="z-10"></footer>
       </div>
+
       <div className={styles.notFoundPage_roundEmptinessVolume} />
+      <Image
+        src="/img/common/skyBg.png"
+        alt="Globe image"
+        className={styles.skyBg}
+        width={1920}
+        height={1080}
+      />
     </>
   )
 }
