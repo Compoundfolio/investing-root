25cbeb0e31ab4323a5bbcd880402e7fa0f1efb12
Maverick17user <maksim.epifanov.2000@mail.ru>
2024-02-02 21:54:34 +0100

    Started with pie chart refactoring and redesign

--- src/components/pages/DashboardPage/components/PortfolioAssetsPieChart/PortfolioAssetsPieChart.tsx
+++ src/components/pages/DashboardPage/components/PortfolioAssetsPieChart/PortfolioAssetsPieChart.tsx
@@ -25,26 +25,43 @@ const getChartDataSet = (openPositions: NormalizedPositions): Data => {
     : []
 }
 
+const HARD_CODED_DATA: DataItem[] = [
+  { id: "k34", label: "TSCO", value: 140 },
+  { id: "k34234", label: "SCO", value: 170 },
+  { id: "k3dfsg4", label: "TSC", value: 140 },
+  { id: "k3dfgx4", label: "TSO", value: 140 },
+  { id: "k3zzc4", label: "TCO", value: 240 },
+  { id: "k3x4", label: "TS", value: 40 },
+  { id: "k3wf4", label: "T", value: 140 },
+  { id: "k3ww4", label: "CO", value: 40 },
+  { id: "k3w4", label: "F", value: 70 },
+  { id: "k34www", label: "AD", value: 340 },
+]
+
 const { darkLightGreen, lightGreen, darkGreen, gold, grayD9 } = colors
 const CHART_COLORS_LIST = [darkLightGreen, lightGreen, darkGreen, gold, grayD9]
 
 const PortfolioAssetsPieChart = () => {
   const [selectedEntityPiePercentage, setSelectedEntityPiePercentage] =
     useState(0)
+  const [selectedEntityLabel, setSelectedEntityLabel] = useState("")
   const { brokerageEntities } = useBrokeragesData()
 
-  const dataSet = useMemo(() => {
-    const assets = brokerageEntities[0]?.getAssets()
-    return getChartDataSet(assets?.openPositions)
-  }, [brokerageEntities])
+  const dataSet = HARD_CODED_DATA
+
+  // const dataSet = useMemo(() => {
+  //   const assets = brokerageEntities[0]?.getAssets()
+  //   return getChartDataSet(assets?.openPositions)
+  // }, [brokerageEntities])
 
   const totalAssetsSum = dataSet?.reduce((previousValue, currentValue) => {
     return previousValue + currentValue.value
   }, 0)
 
-  const handleHover = (d: any) => {
-    const hoveredEntityPiePercentage = (d.value / totalAssetsSum) * 100
+  const handleHover = (item: DataItem) => {
+    const hoveredEntityPiePercentage = (item.value / totalAssetsSum) * 100
     setSelectedEntityPiePercentage(hoveredEntityPiePercentage)
+    setSelectedEntityLabel(item.label)
   }
 
   const [activeItem, setActiveItem] = useState<DataItem | null>(null)
@@ -68,8 +85,16 @@ const PortfolioAssetsPieChart = () => {
   return (
     // <section className={clsx(styles.pieChartContainer, 'relative')} ref={ref}>
     <section className="relative">
-      <div className="absolute flex justify-center w-full text-white top-1/2">
-        <span>{selectedEntityPiePercentage.toFixed(2)}%</span>
+      <div className="absolute flex flex-col items-center justify-center w-full text-white top-1/2 gap-0.5">
+        <span className={styles.pieChart__focusedItem_label}>
+          [ {selectedEntityLabel} ]
+        </span>
+        <span className={styles.pieChart__focusedItem_percentage}>
+          {selectedEntityPiePercentage.toFixed(2)}%
+        </span>
+        <span className={styles.pieChart__focusedItem_label}>
+          [ {selectedEntityLabel} ]
+        </span>
       </div>
       <ChartTypeSwitcher />
       <div className={styles.pieChartContainer}>
@@ -84,6 +109,7 @@ const PortfolioAssetsPieChart = () => {
           onMouseLeave={(e) => console.log(e)}
           onMouseMove={(e) => console.log(e)}
           data={dataSet}
+          // @ts-ignore
           onMouseEnter={handleHover}
           colors={CHART_COLORS_LIST}
           activeInnerRadiusOffset={5}
